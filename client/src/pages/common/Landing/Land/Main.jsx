a5b21dad09379894f29b86cbbe2a442f19fb1cc6
Anurag Behura <behura960@gmail.com>
2024-04-18 12:01:46 +0530

    null

--- client/src/pages/common/Landing/Land/Main.jsx
+++ client/src/pages/common/Landing/Land/Main.jsx
@@ -17,12 +17,7 @@ const Main = () => {
                     <h3>Dive into Quizzes</h3>
                     <p>Explore the realm of knowledge at Quizify.</p>
                     <p>Quizzes that entertain, challenge, and enlighten</p>
-<<<<<<< HEAD
-                    <a href="/login" id='start-button' class='button'>Get Started</a>
-
-=======
                     <button id='button' type='button'><a href="/login">Get Started</a></button>
->>>>>>> 89d5cb46f7adf0719da8a4505baabab4be2ff45c
                 </div>
                 <div className='visual-content'>
                     <img src={image} alt='front pic' height={400} />
